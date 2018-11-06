eaba32b15ec1d5c135567327e4ec7314f1615ac9
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-07 02:01:51 +0700

    [ADD] Word link

---
+++ hellopolarbear/src/Component/Infra.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Infra = `Hello`
+
+const Infra =()=>(
+    <div>
+    <p>Infra PolarBear</p>
+    <p>ตะลุย Linux Command ผสมผสานพลังหยินหยางให้ Nginx ทะยาน Web ด้วยใจเหล็กกล้าผ่านนภาไปสู่ Server
+    </p>
+    </div>
+)
+export default Infra;
