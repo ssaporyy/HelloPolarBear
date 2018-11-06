cb64e7d0d381949e4faaba1702f26a0c5722f5ed
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-06 18:19:13 +0700

    [ADD] page

---
+++ hellopolarbear/src/Component/Where.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Where =()=>(
+    <h1>Where</h1>
+)
+export default Where;
---
+++ hellopolarbear/src/Component/Who.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Who extends React.Component{
+    
+    render(){
+        return(
+            <h1>Who</h1>
+        )
+    }
+}
+export default Who;
