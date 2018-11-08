2779c63f615296cc1f5b92d4c710da01e4d62a4b
ssaporyy <orayasangpheng@gmail.com>
2018-11-08 17:26:55 +0700

    [UPDATE] Landing , Apps

--- hellopolarbear/src/Component/CORE/Apps.js
+++ hellopolarbear/src/Component/CORE/Apps.js
@@ -4,9 +4,10 @@ import '../../App.css';
 import What from '../What/Index';
 import Who from '../Who/Index';
 import Where from '../Where/Index';
-import Contact from '../Contact/index';
+import Contact from '../Contact';
 import Card from './Card'
-import Landing from '../landing/index';
+import Landing from '../landing';
+import styled from 'styled-components'
 
 
 const hello = `Hello`
@@ -15,39 +16,39 @@ const world = `${hello} World`
 const hastag = `#${world} #PolarBear #HelloPolarBear `
 const hastag2 = `#HelloFrontEnd #HelloDesign #HelloGame #HelloInfra`
 
+// const
+
 class App extends Component {
   render() {
     return (
       <div>
         <div className="container bg-dark">
-     
-         <div className="card text-blue bg--blue: #93E8FA;">
-         <h1 className="text-center">{hello} {bear}</h1>
-         
-         
-         
-         <Landing/>
 
-         
-         
-         
-      </div>
-    </div>
-        
-        <p>{hastag}</p>
-        <p>{hastag2}</p>
-        
-        <What/>
-        
-        <Who/>
+          <div className="card text-blue bg--blue" />
+          <h1 className="text-center">{hello} {bear}</h1>
 
-        <Where/>
 
-        <Contact/>
-        
-      </div>
-    );
-  }
-}
 
+          <Landing />
+
+
+
+
+          <p><hastag /></p>
+          <p><hastag2 /></p>
+
+          <What />
+
+          <Who />
+
+          <Where />
+
+          <Contact />
+
+        </div>
+        </div>
+        );
+      }
+    }
+    
 export default App;
--- hellopolarbear/src/Component/landing/index.js
+++ hellopolarbear/src/Component/landing/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const NavBar = styled.nav`
+    background-color: ;
+    color: #fff; 
+`
+
 
 class Landing extends React.Component{
 
     render(){
         return(
-            <nav class="nav nav-pills nav-fill">
+            <NavBar class="nav nav-pills nav-fill">
                 <a class="nav-item nav-link" href="#">Home</a>
                 <a class="nav-item nav-link" href="#">What</a>
                 <a class="nav-item nav-link" href="#">Who</a>
                 <a class="nav-item nav-link" href="#">When</a>
                 <a class="nav-item nav-link" href="#">Contact</a>
-            </nav>
+            </NavBar>
         )
     }
 }
