23b2d162aaaa0c92d019161be86453e626ee26dc
ssaporyy <orayasangpheng@gmail.com>
2018-11-10 01:50:12 +0700

    [ADD] styled for change Topic fonts

--- hellopolarbear/src/PolarBear/PolarBear.js
+++ hellopolarbear/src/PolarBear/PolarBear.js
@@ -9,14 +9,32 @@ const Icon1 = styled.img`
 
 `
 
+const Name = styled.h1`
+    //font-family: 'Shadows Into Light', cursive;
+    font-family: 'Permanent Marker', cursive;
+    text-align:center;
+    color : white;
+    font-size: 90px;
+`
+
+const Name1 = styled.h2`
+    font-family: 'Kalam', cursive;
+    text-align:center;
+    color : white;
+    font-size: 90px;
+    margin: 50px;
+    marginTop: 20px;
+
+`
+
 class PolarBear extends React.Component{
 
     render(){
         return(
         <div>
             <Icon1 img src="static/img/bears icon.png" ></Icon1>
-                <h1 className="text-center">Hello World</h1>
-                <h1>Polar Bear</h1>
+                <Name h1 className="text-center">Hello World</Name>
+                <Name1 h2>Polar Bear</Name1>
         </div>
         )
     }
