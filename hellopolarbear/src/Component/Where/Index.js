99e3b6085cbf0ab454a8671a87079634e5a7bf38
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-09 14:40:14 +0700

    [UPDATE] insert code for image

--- hellopolarbear/src/Component/Where/Index.js
+++ hellopolarbear/src/Component/Where/Index.js
@@ -5,8 +5,8 @@ class Where extends React.Component{
     render(){
         return(
             <div>
-                <h1>Where</h1>
-                <p>ค่ายนี้จัดที่ไหน?</p>
+                <h1>ค่ายนี้จัดที่ไหน?</h1> <img src="static/img/Vector.png" width="76" height="130"/>
+                <br></br><img src="static/img/image.png" width="913" height="637"/>
                 <p>คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
             </div>
         )
