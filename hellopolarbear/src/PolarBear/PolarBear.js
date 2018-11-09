6499beca5a5d29bd1e8e07d25b8790d8725e3188
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-09 14:38:13 +0700

    [ADD] create index polarBear

---
+++ hellopolarbear/src/PolarBear/PolarBear.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class PolarBear extends React.Component{
+
+    render(){
+        return(
+        <div>
+            <img src="static/img/bears icon.png" width="397" height="397"/>
+                <h1 className="text-center">Hello World</h1>
+                <h1>Polar Bear</h1>
+        </div>
+        )
+    }
+}
+export default PolarBear;
