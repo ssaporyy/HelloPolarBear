5bc52f432d4b8177874120b3d40ed3c7a529ee15
ssaporyy <orayasangpheng@gmail.com>
2018-11-10 00:51:18 +0700

    [UPDATE] styled icon bear

--- hellopolarbear/src/PolarBear/PolarBear.js
+++ hellopolarbear/src/PolarBear/PolarBear.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Icon1 = styled.img`
+    width : 250px;
+    height : 250px;
+    margin: 50px;
+
+
+`
 
 class PolarBear extends React.Component{
 
     render(){
         return(
         <div>
-            <img src="static/img/bears icon.png" width="397" height="397"/>
+            <Icon1 img src="static/img/bears icon.png" ></Icon1>
                 <h1 className="text-center">Hello World</h1>
                 <h1>Polar Bear</h1>
         </div>
