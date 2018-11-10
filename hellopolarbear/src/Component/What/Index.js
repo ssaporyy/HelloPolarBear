bd4f4032bacfb13d61d9317d73150e27663795bc
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-10 22:21:30 +0700

    [UPDATE] add code link for icon

--- hellopolarbear/src/Component/What/Index.js
+++ hellopolarbear/src/Component/What/Index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
-
-
 class What extends React.Component{
 
     render(){
         return(
             <div>
                 <h1>ค่ายนี้มีอะไรบ้าง?</h1>
-<p>Hello PorlarBear คือ ค่ายที่จะพามาเจาะลึกชีวิตชาวหมีอย่างพวกเรา นอกจากความรู้และประสบการณ์ด้านไอทีที่น้องจะได้รับ น้อง ๆ จะได้มีโอกาสสัมผัสชีวิตของการเป็นนักไอทีอย่างแท้จริง มีร่วมสร้างโปรเจ็คที่ยิ่งใหญ่ไปด้วยกัน ณ คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
+            <p>Hello PorlarBear คือ ค่ายที่จะพามาเจาะลึกชีวิตชาวหมีอย่างพวกเรา นอกจากความรู้และประสบการณ์ด้านไอทีที่น้องจะได้รับ น้อง ๆ จะได้มีโอกาสสัมผัสชีวิตของการเป็นนักไอทีอย่างแท้จริง มีร่วมสร้างโปรเจ็คที่ยิ่งใหญ่ไปด้วยกัน ณ คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
 สวัสดีหมีขั้วโลกตัวน้อย อากาศหนาวแบบนี้เรามาอยู่รวมกันดีมั้ย เอาล่ะ!! เรามาดูกันเลยดีกว่าว่าจะมีหมีชนิดไหนให้น้องๆได้ร่วมทีมบ้าง</p>
-                <img src="static/img/group6.png" width="241" height="247" />
-                <img src="static/img/group7.png" width="241" height="247" />
-                <img src="static/img/group5.png" width="241" height="247" />
-                <img src="static/img/group2.png" width="241" height="247" />
+            
+                
+            <a href="#" class="alert-link">
+            <img src="static/img/group6.png" width="241" height="247" /></a>
+                
+            <a href="#" class="alert-link">
+            <img src="static/img/group7.png" width="241" height="247" /></a>
+                
+            <a href="#" class="alert-link">
+            <img src="static/img/group5.png" width="241" height="247" /></a>
+                
+            <a href="#" class="alert-link">
+            <img src="static/img/group2.png" width="241" height="247" /></a>
+                                
 <p>ทีนี้น้องๆก็รู้แล้วใช่มั้ยว่าอยากเป็นหมีอะไร อย่าลืมสมัครกันเข้ามาแล้วเราจะร่วมตะลุยความหนาวไปด้วยกัน!!</p>
             </div>
         )
