c81e232b261eb68a75d2d535fadd06486840ef48
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-10 20:29:11 +0700

    [UPDATE] edit component become Component

--- hellopolarbear/src/App.js
+++ hellopolarbear/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Apps from './component/CORE/Apps'
+import Apps from './Component/CORE/Apps'
 
 class App extends Component {
   render() {
