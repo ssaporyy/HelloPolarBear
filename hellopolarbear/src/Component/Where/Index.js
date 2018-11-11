38981cad4568703b291bfc0229c104c00fcaa797
ssaporyy <orayasangpheng@gmail.com>
2018-11-11 15:35:44 +0700

    [ADD] styled

--- hellopolarbear/src/Component/Where/Index.js
+++ hellopolarbear/src/Component/Where/Index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Direct = styled.img`
+    width: 76px;
+    height: 130px;
+
+`
+
+
+
 
 
 class Where extends React.Component{
@@ -8,7 +16,8 @@ class Where extends React.Component{
     render(){
         return(
             <div className="my-5">
-                <h1>ค่ายนี้จัดที่ไหน?</h1> <img src="static/img/Vector.png" width="76" height="130"/>
+                <h1>ค่ายนี้จัดที่ไหน?</h1> 
+                <Direct className="m-5" src="static/img/Vector.png" />
                 <br></br><img src="static/img/image.png" width="913" height="637"/>
                 <p>คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
             </div>
