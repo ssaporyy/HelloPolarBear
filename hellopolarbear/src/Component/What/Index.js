11f753016cc50c49a7795dc54b87226e71e28085
Ying <yaowanat.yy@mail.kmutt.ac.th>
2018-11-12 00:33:14 +0700

    [UPDATE] add box

--- hellopolarbear/src/Component/What/Index.js
+++ hellopolarbear/src/Component/What/Index.js
@@ -26,8 +26,6 @@ const Ice4 = styled.img`
     width : 140px;
     height : 140px;
     margin: 20px;
-
-
 `
 
 const What1 = styled.h1`
@@ -37,38 +35,49 @@ const What1 = styled.h1`
 
 const Ice = styled.div`
     margin: 20px;
-
+`
+const Box = styled.div`
+    background-color: rgba(146, 187, 230, 0.54);
+    border: 0xp solid transparent;
+    border-radius: 2em;
 `
 
 
-class What extends React.Component{
-    
+class What extends React.Component {
 
-    render(){
-        return(
-            <div>
-                <What1><h1>ค่ายนี้มีอะไรบ้าง?</h1></What1>
-            <p>Hello PorlarBear คือ ค่ายที่จะพามาเจาะลึกชีวิตชาวหมีอย่างพวกเรา นอกจากความรู้และประสบการณ์ด้านไอทีที่น้องจะได้รับ น้อง ๆ จะได้มีโอกาสสัมผัสชีวิตของการเป็นนักไอทีอย่างแท้จริง มีร่วมสร้างโปรเจ็คที่ยิ่งใหญ่ไปด้วยกัน ณ คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
-สวัสดีหมีขั้วโลกตัวน้อย อากาศหนาวแบบนี้เรามาอยู่รวมกันดีมั้ย เอาล่ะ!! เรามาดูกันเลยดีกว่าว่าจะมีหมีชนิดไหนให้น้องๆได้ร่วมทีมบ้าง</p>
-            
-        <div class="row">
-            <div class="col-6">
-                <Ice1 img src="static/img/group6.png"></Ice1>
-            <Ice className="my-60">
-                <Ice2 img src="static/img/group5.png" ></Ice2>
-            </Ice>
-            </div>     
 
-            <div class="col-6">
-                <Ice1 img src="static/img/group7.png"></Ice1>
-                <Ice className="my-60">
-                <Ice2 img src="static/img/group2.png" ></Ice2>
-            </Ice>
-            </div>
-        </div>
-                
-                                
-<p>ทีนี้น้องๆก็รู้แล้วใช่มั้ยว่าอยากเป็นหมีอะไร อย่าลืมสมัครกันเข้ามาแล้วเราจะร่วมตะลุยความหนาวไปด้วยกัน!!</p>
+    render() {
+        return (
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-2" />
+                    <div className="col-12">
+                        <Box className="mb-2 p-2">
+                            <What1><h1>ค่ายนี้มีอะไรบ้าง?</h1></What1>
+                            <p>Hello PorlarBear คือ ค่ายที่จะพามาเจาะลึกชีวิตชาวหมีอย่างพวกเรา นอกจากความรู้และประสบการณ์ด้านไอทีที่น้องจะได้รับ</p>
+                            <p> น้อง ๆ จะได้มีโอกาสสัมผัสชีวิตของการเป็นนักไอทีอย่างแท้จริง มีร่วมสร้างโปรเจ็คที่ยิ่งใหญ่ไปด้วยกัน</p>
+                            <p>ณ คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
+                            <p>อากาศหนาวแบบนี้เรามาอยู่รวมกันดีมั้ย เอาล่ะ!! เรามาดูกันเลยดีกว่าว่าจะมีหมีชนิดไหนให้น้องๆได้ร่วมทีมบ้าง</p>
+                        </Box>
+                    </div>
+                    <div className="col-2" />
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <Ice1 img src="static/img/group6.png"></Ice1>
+                        <Ice className="my-60">
+                            <Ice2 img src="static/img/group5.png" ></Ice2>
+                        </Ice>
+                    </div>
+
+                    <div class="col-6">
+                        <Ice1 img src="static/img/group7.png"></Ice1>
+                        <Ice className="my-60">
+                            <Ice2 img src="static/img/group2.png" ></Ice2>
+                        </Ice>
+                    </div>
+                </div>
             </div>
         )
     }
