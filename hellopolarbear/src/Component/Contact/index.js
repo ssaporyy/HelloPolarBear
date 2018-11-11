3a7a7513c50647942cf77e1221d50fcb316f5888
ssaporyy <orayasangpheng@gmail.com>
2018-11-12 01:34:27 +0700

    [ADD]Navbar

--- hellopolarbear/src/Component/Contact/index.js
+++ hellopolarbear/src/Component/Contact/index.js
@@ -1,4 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Face = styled.img`
+    width: ;
+    height: ;
+
+`
+
+const NavBar = styled.nav`
+    background-color: #93e8fa ;
+    background-attachment: ;
+    
+`
+
+const Link = styled.a`
+    color: #fff;
+    font-size: 25px;
+    margin: 10px;
+    &:hover{
+        color:#000;
+        text-decoration:none;
+    }
+`
 
 class Contact extends React.Component{
 
@@ -6,6 +29,21 @@ class Contact extends React.Component{
         return(
             <div>
                 <h1>Contact</h1>
+                <NavBar className="nav nav-pills nav-fill ">
+                
+                <Link className="nav-item" >
+                    
+                </Link>
+                <Link className="nav-item" >
+                   
+                </Link>
+                <Link className="nav-item " >
+                   
+                </Link>
+                <Link className="nav-item " >
+                    
+                </Link>
+            </NavBar>
                 
             </div>
         )
